d26592af6f58a28ad05622b5cf9341fbe87f88dd
Andreas Kjellqvist <a15andkj@student.his.se>
2016-01-21 18:08:00 +0100

    Added Entity
    Started on sprint 1 by adding Entity.

---
+++ Source/Luddis_Project/Luddis/Header/Entity.h
@@ -0,0 +1,22 @@
+#ifndef INCLUDED_ENTITY
+#define INCLUDED_ENTITY
+
+#include <SFML\Graphics\Transformable.hpp>
+#include <SFML\Graphics\Drawable.hpp>
+
+// Framåtdeklaration
+namespace sf{
+	class Clock;
+}
+
+class Entity : public sf::Transformable, public sf::Drawable {
+public:
+	Entity();
+	virtual ~Entity();
+
+	// Funktion för att uppdatera entiteten
+	virtual void tick(const sf::Clock& gameTime) = 0;
+	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const = 0;
+};
+
+#endif
