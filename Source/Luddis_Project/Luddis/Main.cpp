fe2a726aa4540fc52519dc4235cfff4ee5249ecf
Andreas Kjellqvist <a15andkj@student.his.se>
2016-01-22 22:59:50 +0100

    Removed example code from main. Removed copy and = from ResourceManager to avoid bypassing singleton

--- Source/Luddis_Project/Luddis/Main.cpp
+++ Source/Luddis_Project/Luddis/Main.cpp
@@ -1,20 +1,31 @@
 #include <SFML/Graphics.hpp>
 
 int main(){
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+	
+	// TODO: Use gamemanager
 
-	while (window.isOpen()) {
-		sf::Event event;
-		while (window.pollEvent(event)) {
-			if (event.type == sf::Event::Closed)
-				window.close();
-		}
+	//sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!", sf::Style::Fullscreen);
 
-		window.clear();
-		window.draw(shape);
-		window.display();
+	//while (window.isOpen()) {
+	//	sf::Event event;
+	//	while (window.pollEvent(event)) {
+	//		switch (event.type){
+	//		case sf::Event::Closed:
+	//			window.close();
+	//			break;
+	//		case sf::Event::KeyPressed:
+	//			if (event.key.code == sf::Keyboard::Escape){
+
+	//			}
+	//			break;
+	//		}
+	//	}
+
+	//	window.clear();
+
+	//	// Draw
+	//	
+	//	window.display();
 	}
 
 	return 0;
