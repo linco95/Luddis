0e0f85311ca711be383b6f4d10bff6d7d2deee9b
Bearmachine <wahlberg.bjorn@hotmail.com>
2016-01-24 21:48:00 +0100

    Revert "Separated the ResourceManager into a SoundManager and GraphicManager."
    This reverts commit 43ef0d070409b2a49a207af864f66b8a809fe1f1.

--- Source/Luddis_Project/Luddis/ResourceManager.h
+++ Source/Luddis_Project/Luddis/ResourceManager.h
@@ -4,18 +4,47 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 #include <string>
-#include "GraphicManager.h"
-#include "SoundManager.h"
 
-class ResourceManager: public GraphicManager, public SoundManager{
+class ResourceManager{	
 public:
 	static ResourceManager& getInstance();
 	~ResourceManager();
 
+	//Texture related functions
+	sf::Texture& getTexture(std::string filename);
+	void loadTexture(std::string filename/*, sf::Rect = sf::IntRect*/);
+	void clearTexture(std::string filename);
+	void clearAllTextures();
+
+	//Font related functions
+	sf::Font& getFont(std::string filename);
+	void loadFont(std::string filename);
+	void clearFont(std::string filename);
+	void clearAllFonts();
+
+	//SoundBuffer related functions
+	sf::SoundBuffer& getSoundBuffer(std::string filename);
+	void loadSoundBuffer(std::string filename);
+	void clearSoundBuffer(std::string filename);
+	void clearAllSoundBuffers();
+
+	//Music related functions
+	sf::Music& getMusic(std::string filename);
+	void loadMusic(std::string filename);
+	void clearMusic(std::string filename);
+	void clearAllMusic();
+
 private:
 	ResourceManager();
-	ResourceManager(ResourceManager& rM);
 	ResourceManager& operator=(ResourceManager& rM);
+	typedef std::vector<std::pair<sf::Texture*, std::string>> TexturePairVector;
+	TexturePairVector mTextures;
+	typedef std::vector<std::pair<sf::Font*, std::string>> FontPairVector;
+	FontPairVector mFonts;
+	typedef std::vector<std::pair<sf::SoundBuffer*, std::string>> SoundBufferPairVector;
+	SoundBufferPairVector mSoundBuffers;
+	typedef std::vector<std::pair<sf::Music*, std::string>> MusicPairVector;
+	MusicPairVector mMusic;
 };
 
 #endif
