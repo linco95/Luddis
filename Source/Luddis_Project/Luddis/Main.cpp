2f6b4735deed69a58d7ce57410eeff6ac9d4c217
Andreas Kjellqvist <a15andkj@student.his.se>
2016-02-02 21:05:00 +0100

    Started testing out the JSON parser

--- Source/Luddis_Project/Luddis/Main.cpp
+++ Source/Luddis_Project/Luddis/Main.cpp
@@ -1,11 +1,18 @@
 #include "GameManager.h"
 
-#include <SFML\Audio.hpp>
+#include <rapidjson\document.h>
+#include <iostream>
 
-using namespace sf;
+using namespace rapidjson;
+
+void testingJSON(){
+	const char* json = "{ \"hello\": \"world\", \"t\" : true, \"f\" : false, \"n\" : null, \"i\" : 123, \"pi\" : 3.1416, \"a\" : [1, 2, 3, 4] }";
+	std::cout << "Original string: " << json << std::endl;
+
+}
 
 int main(){
-
+	testingJSON();
 	GameManager::getInstance().run();
 
 	return 0;
