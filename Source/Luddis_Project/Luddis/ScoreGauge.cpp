23411ef2fec27df856763e9f71c7224f86682c0c
Bearmachine <wahlberg.bjorn@hotmail.com>
2016-02-13 23:47:49 +0100

    Missed the files for the score gauge.

---
+++ Source/Luddis_Project/Luddis/ScoreGauge.cpp
@@ -0,0 +1,57 @@
+#include "ScoreGauge.h"
+#include "Inventory.h"
+#include "ResourceManager.h"
+
+ScoreGauge::ScoreGauge(sf::RenderWindow* aWindow, std::string backgroundFilename, std::string gaugeFilename, sf::Vector2f screenPos) :
+mIsActive(true),
+mIsAlive(true),
+mInventory(&Inventory::getInstance()),
+mResourceManager(&ResourceManager::getInstance()),
+mBackground(mResourceManager->getTexture(backgroundFilename)),
+mGauge(mResourceManager->getTexture(gaugeFilename)),
+mGaugeRectSize(mGauge.getTextureRect()){
+	setPosition(screenPos);
+	setOrigin(0, (float)mGaugeRectSize.height / 2);
+}
+
+ScoreGauge::~ScoreGauge(){
+
+}
+
+void ScoreGauge::tick(const sf::Time& deltaTime){
+	updateGauge();
+}
+
+void ScoreGauge::draw(sf::RenderTarget& target, sf::RenderStates states) const{
+	states.transform *= getTransform();
+	target.draw(mBackground, states);
+	target.draw(mGauge, states);
+}
+
+bool ScoreGauge::isAlive() const{
+	return mIsAlive;
+}
+
+bool ScoreGauge::isActive() const{
+	return mIsActive;
+}
+
+void ScoreGauge::setActive(const bool& active){
+	mIsActive = active;
+}
+
+ScoreGauge::RenderLayer ScoreGauge::getRenderLayer() const{
+	return FOREGROUND;
+}
+
+void ScoreGauge::updateGauge(){
+	if (mInventory->getMaxDust() != 0){
+		int maxDust = mInventory->getMaxDust();
+		int currentDust = mInventory->getDust();
+		int offset = maxDust / 3;
+		int gaugeWidth = (int)((float)mGaugeRectSize.width * ((float)(currentDust+offset) / (float)(maxDust+offset)));
+		int gaugeHeight = mGaugeRectSize.height;
+		sf::IntRect gaugeRect(0, 0, gaugeWidth, gaugeHeight);
+		mGauge.setTextureRect(gaugeRect);
+	}
+}
