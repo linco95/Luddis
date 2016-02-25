1112e59548c075d1adbd25f9cc302fabe277da14
Andreas Kjellqvist <a15andkj@student.his.se>
2016-02-25 10:57:55 +0100

    Saving log to a file.
    Made debug class portable
    
    Commented out for now

--- Source/Luddis_Project/Luddis/Debug.cpp
+++ Source/Luddis_Project/Luddis/Debug.cpp
@@ -1,9 +1,19 @@
 #include "Debug.h"
 #include <iostream>
 #include <SFML\Graphics\Shape.hpp>
+#include <string>
+#include <fstream>
+#include <ctime>
+#include <sstream>
+
+static const char* SAVEPATH = "Logs/";
+
+
+#ifdef _WIN32
 namespace WINDOWS{
 #include <windows.h>
 }
+#endif
 using namespace std;
 struct ErrorInfo{
 	ErrorInfo(char* aString, const WINDOWS::WORD& aColor) : 
@@ -30,6 +40,7 @@ const ErrorInfo getErrorLevelStr(const Debug::ERRORLEVEL& aErrLvl){
 		return ErrorInfo(" NOT DEFINED ", 0x0F);
 	}
 }
+#ifdef _WIN32
 namespace WINDOWS{
 	void printInColor(const WORD& aColor, const char* aText) {
 	
@@ -47,10 +58,53 @@ namespace WINDOWS{
 		SetConsoleTextAttribute(hstdout, csbi.wAttributes);
 	}
 }
+#endif
+
+void Debug::finalize() {
+	/*mLog.
+	if (mLog.message.empty() || mLogname.empty()) return;
+	ofstream saveFile;
+	string savepath = SAVEPATH;
+
+	saveFile.open(savepath);*/
+	
+}
+
+void Debug::initialize() {
+	//
+	//if (mLog.message.empty()) return;
+	//time_t currentTime;
+	//struct tm *localTime;
+
+	//time(&currentTime);                   // Get the current time
+	//localTime = localtime(&currentTime);  // Convert the current time to the local time
+
+	//int day = localTime->tm_mday;
+	//int month = localTime->tm_mon + 1;
+	//int year = localTime->tm_year + 1900;
+	//int hour = localTime->tm_hour;
+	//int min = localTime->tm_min;
+	//int sec = localTime->tm_sec;
+
+
+
+	//mLog = "Luddis | Started " + day;
+}
+
 void Debug::log(const string& aMsg, const ERRORLEVEL& aLvl){
 #if defined _DEBUG || LOG_IN_RELEASE
+	string message = "[";
+	message += getErrorLevelStr(aLvl).asString;
+	message =+ "]: " + aMsg;
+	mLog += message;
+
+#ifdef _WIN32
 	cout << "[";
 	WINDOWS::printInColor(getErrorLevelStr(aLvl).color, getErrorLevelStr(aLvl).asString);
 	cout <<  "]: " << aMsg << endl;
+#else
+	cout << message << endl;
+#endif
+
 #endif
 }
