3590a3086714559f2ed5cc99bed2cd6bc82df7af
gb22 <blackdom20@gmail.com>
2016-03-09 17:06:46 +0100

    Non working BossRobot. Testing part commented in Level

--- Source/Luddis_Project/Luddis/LuddisStatePlayable.cpp
+++ Source/Luddis_Project/Luddis/LuddisStatePlayable.cpp
@@ -221,8 +221,8 @@ void LuddisStatePlayable::updateRotation(){
 void LuddisStatePlayable::changeScale() {
 	//int dust = Inventory::getInstance().getDust();
 	//int max = Inventory::getInstance().getMaxDust();
-	float percentDust = (float)(Inventory::getInstance().getDust() / Inventory::getInstance().getMaxDust());
-	if (percentDust < 20.0f && mScale != sf::Vector2f(1.0f, 1.0f)) {
+	float percentDust = float((Inventory::getInstance().getDust() / Inventory::getInstance().getMaxDust()) * 100);
+	if (percentDust < 19.9f && mScale != sf::Vector2f(1.0f, 1.0f)) {
 		mScale = { 1.0f , 1.0f };
 		mPlayerPtr->getAnimation()->setDefaultAnimation(ANIMATION_ALMOSTDEAD);
 	}
